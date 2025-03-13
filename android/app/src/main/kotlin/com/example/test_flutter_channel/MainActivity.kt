75ecad7996d227addc9ab845fe1200f31022c34a
Sealight Clock <chbebram@hotmail.com>
2025-03-13 15:03:49 -0700

    2nd commit: Used a platform method channel to get Android battery level.

--- android/app/src/main/kotlin/com/example/test_flutter_channel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_flutter_channel/MainActivity.kt
@@ -1,5 +1,38 @@
 package com.example.test_flutter_channel
 
+import android.content.Context
+import android.os.BatteryManager
+import android.os.Build
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "com.example.battery"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+                if (batteryLevel != -1) {
+                    result.success(batteryLevel)
+                } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            -1
+        }
+    }
+}
